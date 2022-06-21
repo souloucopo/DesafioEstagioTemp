44cff52d9bacde2131358a7cdf17094621fa7738
Rodrigo Eduardo <89258381+souloucopo@users.noreply.github.com>
2022-06-20 22:43:40 -0300

    Add files via upload

---
+++ Teste estágio - Temperatura/main.py
@@ -0,0 +1,20 @@
+import statistics
+qtdConjunto = int(input("Qual o tamanho do conjunto?"))
+temperaturas = []
+
+for  i in range(1,qtdConjunto+1) :
+ valores = int(input('Insira o ' + str(i) +'º valor:'))
+ temperaturas.append(valores)
+
+
+media = int(sum(temperaturas)/len(temperaturas))
+
+mediana = statistics.median(temperaturas)
+
+print("Temperaturas antes da ordem:" + str(temperaturas) )
+
+print("Temperaturas ordenadas" + str(sorted(temperaturas)) + ' K')
+
+print('Mediana - ' + str(mediana)+' K')
+
+print('Media - '+ str((media)) + ' K')
